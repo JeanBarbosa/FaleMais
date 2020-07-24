42073a4eab880e33c6b2f1cacb60a533da168f54
Jean Barbosa <programmer.jean@gmail.com>
2020-07-24 02:39:30 -0300

    feat(calc): interface da calculadora dos planos implementada

--- web/src/pages/Falemais/index.js
+++ web/src/pages/Falemais/index.js
@@ -5,25 +5,13 @@ import Autocomplete from '@material-ui/lab/Autocomplete';
 import Radio from '@material-ui/core/Radio';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
-import PGCN from '../../services/PGCN';
+import citys from '../../services/PGCN';
 
 import './styles.css';
 import logoImg from '../../assets/falemais.png';
 
 
 export default function Register() {
-
-  const top100Films = [
-    { title: 'The Shawshank Redemption', year: 1994 },
-    { title: 'The Godfather', year: 1972 },
-    { title: 'The Godfather: Part II', year: 1974 },
-    { title: 'The Dark Knight', year: 2008 },
-    { title: '12 Angry Men', year: 1957 },
-    { title: "Schindler's List", year: 1993 },
-    { title: 'Pulp Fiction', year: 1994 },
-    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
-    { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  ];
 
   const marks = [
     {
@@ -36,11 +24,11 @@ export default function Register() {
     },
     {
       value: 40,
-      label: '30 min',
+      label: '30 min',  
     },
     {
       value: 100,
-      label: '+ 100 min ',
+      label: '+100',
     },
   ];
 
@@ -59,7 +47,7 @@ export default function Register() {
   }
 
   return (
-    <div className="register-container">
+    <div className="calc-container">
         <div className="content">
         
             <form onSubmit={hadleRegister}>
@@ -68,54 +56,60 @@ export default function Register() {
 
             <Autocomplete
               id="combo-box-demo"
-              options={top100Films}
-              getOptionLabel={(option) => option.title}
+              options={citys}
+              getOptionLabel={(city) => city.name}
               style={{ width: 300 }}
               renderInput={(params) => <TextField {...params} label="Ligação de:" variant="outlined" />}
             />
 
             <Autocomplete
               id="combo-box-demo2"
-              options={top100Films}
-              getOptionLabel={(option) => option.title}
+              options={citys}
+              getOptionLabel={(city) => city.name}
               style={{ width: 300 }}
               renderInput={(params) => <TextField {...params} label="Para:" variant="outlined" />}
             />
 
           </div>
-            
-           
-          <Typography id="discrete-slider-small-steps" gutterBottom>
-            Minutos
-          </Typography>
+          
+          <div className="min">
+            <Typography id="discrete-slider-small-steps" gutterBottom>
+              Minutos
+            </Typography>
 
-          <Slider
-            defaultValue={1}
-            getAriaValueText={valuetext}
-            aria-labelledby="discrete-slider-small-steps"
-            step={1}
-            marks={marks}
-            min={1}
-            max={100}
-            valueLabelDisplay="on"
-          />
+            <div className="slider">
+              <Slider
+                defaultValue={1}
+                getAriaValueText={valuetext}
+                aria-labelledby="discrete-slider-small-steps"
+                step={1}
+                marks={marks}
+                min={1}
+                max={100}
+                valueLabelDisplay="auto"
+              />
 
-              <div className="input-group">
+            </div>
+          </div>
 
-              <div>
-              <Radio
-                  checked={selectedValue === 'a'}
-                  onChange={handleChange}
-                  value="a"
-                  name="radio-button-demo"
-                  inputProps={{ 'aria-label': 'A' }}
-                /> 
+          <div className="input-group">
 
-                FaleMais 20
+              <div className="card">
+                <Radio
+                    checked={selectedValue === 'a'}
+                    onChange={handleChange}
+                    value="a"
+                    name="radio-button-demo"
+                    inputProps={{ 'aria-label': 'A' }}
+                  /> 
+
+                  <div>
+                    FaleMais 20
+                  </div>
                 
               </div>
             
-              <div>
+              <div className="card">
                 <Radio
                   checked={selectedValue === 'b'}
                   onChange={handleChange}
@@ -124,10 +118,12 @@ export default function Register() {
                   inputProps={{ 'aria-label': 'B' }}
                 />
 
-                FaleMais 60
+                  <div>
+                    FaleMais 60
+                  </div>
                 </div>
 
-                <div>
+                <div className="card">
                 <Radio
                   checked={selectedValue === 'c'}
                   onChange={handleChange}
@@ -135,7 +131,9 @@ export default function Register() {
                   name="radio-button-demo"
                   inputProps={{ 'aria-label': 'C' }}
                 />
-                  FaleMais 120
+                  <div>
+                    FaleMais 120
+                  </div>
                 </div>
 
               </div>
---
+++ web/src/services/tariff.js
@@ -0,0 +1,10 @@
+const tariff = Object.freeze({
+    '1116': Symbol(1.90),
+    '1611': Symbol(2.90),
+    '1117': Symbol(1.70),
+    '1711': Symbol(2.70),
+    '1118': Symbol(0.90),
+    '1811': Symbol(1.90),
+});
+
+export default tariff;
